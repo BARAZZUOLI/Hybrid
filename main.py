64c0fc69b9cae1cd88054ae36048c45c8cc149ad
BARAZZUOLI <lenny3ba@gmail.com>
2025-12-25 14:43:55 +0100

    djouher test

--- main.py
+++ main.py
@@ -12,53 +12,59 @@ class MachineState:
         #Condition initial
         self.state = '0' 
         self.x = 0 
-        self.y=0 
-        self.z = 20  
+        self.z = 50  
+        self.v = 0
+        self.theta = 0 
+        ###############
+        self.m=10
+        self.Rx=5
+        self.U=10
         #LIST_DATA
-        self.x_data = [] 
-        self.y_data = []  
-        self.z_data = []  
+        self.x_data = []  
+        self.z_data = [] 
+        self.v_data = []  
+        self.theta_data = []  
         #EVENEMENT
         self.e1 = Event(env)
         self.e2 = Event(env)  
         self.e3 = Event(env) 
         self.e4 = Event(env) 
         self.e5 = Event(env) 
-      
+     
     def dynamique_continu(self):
         while True:
 
-            if self.state == '0':
-                self.x += 0.1
-                self.z = self.z 
-                self.y = 0
-                if self.x >= 10:
-                    self.e1.succeed()  
-                    
-            if self.state == '1':
-                self.x += 0.1
-                self.z -=(1/self.x)
-                self.y += 0
-                if self.z <= 10:
-                  self.e2.succeed()  
+            match self.state:
+                case'0':
+                    self.x+=self.v
+                    self.z =self.z
+                    self.v +=(1/self.m)*(self.U-self.Rx)
+                    self.theta=0
+                    if self.x>=20 :
+                        self.e1.succeed()
 
-            if self.state == '2':
-                self.x +=0.1
-                self.z -=(1/self.x)
-                self.y =0
-                if self.z <=0:
-                     self.e3.succeed()  
+                case'1':
+                    self.theta=-20
+                    self.x+=self.v*np.cos(self.theta)
+                    self.z +=self.v*np.sin(self.theta)
+                    self.v +=(1/self.m)*((self.U-self.Rx)-(self.m*9.81*np.sin(self.theta)))
+                    if self.x>=50 :
+                        self.e2.succeed()
+                 
+                case'2':
+                    self.theta=0
+                    self.x+=self.v*np.cos(self.theta)
+                    self.z +=self.v*np.sin(self.theta)
+                    self.v +=0
+                case'3':
+                    a=0
+                case _:
+                    a=0
 
-            if self.state == '3':
-                self.x +=0.1
-                self.z +=1/self.x
-                self.y = 0
-                    
-                    
-              
             self.x_data.append((self.env.now, self.x))
-            self.y_data.append((self.env.now, self.y))
             self.z_data.append((self.env.now, self.z))
+            self.v_data.append((self.env.now, self.x))
+            self.theta_data.append((self.env.now, self.z))
             yield self.env.timeout(0.1) 
 
     def automate(self):
@@ -93,11 +99,10 @@ def main():
     env.process(machine.automate())
     env.process(machine.dynamique_continu())
 
-    env.run(until=100)  
+    env.run(until=500)  
 
     time_points = [point[0] for point in machine.x_data]
     x_values = [point[1] for point in machine.x_data]
-    y_values = [point[1] for point in machine.y_data]
     z_values = [point[1] for point in machine.z_data]
 
 
@@ -105,15 +110,11 @@ def main():
     fig = plt.figure(figsize=(15, 10))
 
 
-    plt.subplot(2, 2, 1)
-    plt.plot(x_values, z_values)
+    plt.subplot(1, 1, 1)
+    plt.plot(x_values, z_values,marker='o')
     plt.xlabel('x values')
     plt.ylabel('z values')
     plt.grid(True)
-
-
-
-# Afficher tous les graphiques
     plt.tight_layout()
     plt.show()
 
