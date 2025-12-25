92ba0d7af4ee5ca372324c1a6abd36dbcf7740cb
BARAZZUOLI <lenny3ba@gmail.com>
2025-12-25 20:52:38 +0100

    mise à jour

--- main.py
+++ main.py
@@ -6,48 +6,51 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class MachineState:
     def __init__(self, env):
-        
+
         #ENVIRONEMENT
         self.env = env
 
         #CONDITION INITIAL
-        self.state = '0' 
+        self.mode = '0' 
+
+        
         self.x = 0 
         self.xdot=0
         self.z = 50 
         self.zdot=0 
         self.v = 0
         self.vdot=0
-        self.theta = 0
-        self.thetadot=0
+      
+
 
         self.m=10
         self.Rx=5
         self.U=10
 
+
         self.x_data = []
         self.xdot_data = []   
         self.z_data = [] 
         self.zdot_data = [] 
-        self.v_data = []  
-        self.theta_data = [] 
-
+        self.v_data = []
+        self.vdot_data = []    
+    
         #EVENEMENT
         self.e1 = Event(env)
-        self.e2 = Event(env)  
-        self.e3 = Event(env) 
+        self.e2 = Event(env)   
+        self.e3 = Event(env)
         self.e4 = Event(env) 
         self.e5 = Event(env) 
      
     def dynamique_continu(self):
         while True:
 
-            match self.state:
+            match self.mode:
                 case'0':
                     self.xdot+=self.v
                     self.zdot =0
                     self.vdot+=(1/self.m)*(self.U-self.Rx)
-                    self.theta=0
+                    self.thetadot =0#à modifié
                     if self.x>=20 :
                         self.e1.succeed()
 
@@ -70,27 +73,26 @@ class MachineState:
             self.x_data.append((self.env.now, self.x))
             self.z_data.append((self.env.now, self.z))
             self.v_data.append((self.env.now, self.x))
-            self.theta_data.append((self.env.now, self.z))
             yield self.env.timeout(1) 
 
     def automate(self):
 
         while True:
-            match self.state:
+            match self.mode:
                 case '0':
                     print(f'Time: [{self.env.now}] --> state[0] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e1
-                    self.state = '1'
+                    self.mode = '1'
 
                 case '1':
                     print(f'Time: [{self.env.now}] --> state[1] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e2
-                    self.state = '2'
+                    self.mode = '2'
 
                 case '2':
                     print(f'Time: [{self.env.now}] --> state[2] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e3
-                    self.state = '3'
+                    self.mode = '3'
 
                 case '3':
                     print(f'Time: [{self.env.now}] --> state[3] [x: {self.x:.2f} z: {self.z:.2f}]')
