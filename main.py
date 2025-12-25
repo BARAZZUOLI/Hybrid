2e80ebc49a41dc6e4c884b4c4ffc0da993a59e1a
BARAZZUOLI <lenny3ba@gmail.com>
2025-12-25 21:58:42 +0100

    lok

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.differentiate import derivative
 
-class MachineState:
+class Hybrid_SYSTEM:
     def __init__(self, env):
 
         #ENVIRONEMENT
@@ -20,14 +21,11 @@ class MachineState:
         self.Ax=0
         self.Az=0
        
-     
         self.m=10
         self.Rx=5
         self.U=10
         self.Rz=5
 
-
-      
         self.x_data=[]
         self.z_data=[]
         self.Vx_data=[]
@@ -48,23 +46,30 @@ class MachineState:
 
             match self.mode:
                 case'0':
-                    self.Vx=self.Vx
-                    self.Vz=self.Vz
-                    self.Ax=(1/self.m)*(self.U-self.Rx)
-                    self.Az=(1/self.m)*(self.Rz-self.m*9.81)
-                    self.Vx+=self.x
-                    if self.x>=20 :
+                    self.x=self.env.now
+                    self.z=50
+                    self.Vx=5
+                    self.Vz=0
+                    if self.x>=20:
                         self.e1.succeed()
 
-                    
-            
-                    
-                    
-            
+                case'1':
+                    self.x=self.env.now
+                    self.z-=self.x
+                    self.Vx=5
+                    self.Vz=-3
+                    if self.z==0:
+                        self.e2.succeed()
 
+                case'2':
+                    self.x=5*self.env.now
+                    self.z=0
+                    self.Vx=5
+                    self.Vz=0
+    
             self.x_data.append((self.env.now, self.x))
             self.z_data.append((self.env.now, self.z))
-            self.v_data.append((self.env.now, self.x))
+            
             yield self.env.timeout(1) 
 
     def automate(self):
@@ -72,17 +77,17 @@ class MachineState:
         while True:
             match self.mode:
                 case '0':
-                    print(f'Time: [{self.env.now}] --> state[0] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    print(f'Time: [{self.env.now}] --> mode[0] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e1
                     self.mode = '1'
 
                 case '1':
-                    print(f'Time: [{self.env.now}] --> state[1] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    print(f'Time: [{self.env.now}] --> mode[1] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e2
                     self.mode = '2'
 
                 case '2':
-                    print(f'Time: [{self.env.now}] --> state[2] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    print(f'Time: [{self.env.now}] --> mode[2] [x: {self.x:.2f} z: {self.z:.2f}]')
                     yield self.e3
                     self.mode = '3'
 
@@ -95,14 +100,14 @@ class MachineState:
 def main():
 
     env = Environment()
-    machine = MachineState(env)
-
-    env.process(machine.automate())
+    machine = Hybrid_SYSTEM(env)
     env.process(machine.dynamique_continu())
+    env.process(machine.automate())
+
 
     env.run(until=100)  
 
-    time_points = [point[0] for point in machine.x_data]
+    temps_point = [point[1] for point in machine.x_data]
     x_values = [point[1] for point in machine.x_data]
     z_values = [point[1] for point in machine.z_data]
 
@@ -110,7 +115,7 @@ def main():
        
     fig = plt.figure(figsize=(15, 10))
     plt.subplot(1, 1, 1)
-    plt.plot(x_values, z_values,marker='o')
+    plt.plot(temps_point,z_values ,marker='o')
     plt.xlabel('x values')
     plt.ylabel('z values')
     plt.grid(True)
