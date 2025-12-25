62a5cf6a68b0cf7a0682567701ab6c58253a361d
BARAZZUOLI <lenny3ba@gmail.com>
2025-12-25 20:16:56 +0100

    mise à jour

--- main.py
+++ main.py
@@ -6,20 +6,17 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class MachineState:
     def __init__(self, env):
-
         #ENVIRONEMENT
         self.env = env
-        #Condition initial
+        #CONDITION INITIAL
         self.state = '0' 
         self.x = 0 
         self.z = 50  
         self.v = 0
         self.theta = 0 
-        ###############
         self.m=10
         self.Rx=5
         self.U=10
-        #LIST_DATA
         self.x_data = []  
         self.z_data = [] 
         self.v_data = []  
@@ -44,7 +41,7 @@ class MachineState:
                         self.e1.succeed()
 
                 case'1':
-                    self.theta=-20
+                    self.theta=-1
                     self.x+=self.v*np.cos(self.theta)
                     self.z +=self.v*np.sin(self.theta)
                     self.v +=(1/self.m)*((self.U-self.Rx)-(self.m*9.81*np.sin(self.theta)))
@@ -56,6 +53,7 @@ class MachineState:
                     self.x+=self.v*np.cos(self.theta)
                     self.z +=self.v*np.sin(self.theta)
                     self.v +=0
+                    
                 case'3':
                     a=0
                 case _:
@@ -65,31 +63,32 @@ class MachineState:
             self.z_data.append((self.env.now, self.z))
             self.v_data.append((self.env.now, self.x))
             self.theta_data.append((self.env.now, self.z))
-            yield self.env.timeout(0.1) 
+            yield self.env.timeout(1) 
 
     def automate(self):
+
         while True:
-            if self.state == '0':
-                print(f'Time: [{self.env.now}] --> state[0] [x: {self.x:.2f} z: {self.z:.2f}]')
-                yield self.e1 
-                self.state='1'
+            match self.state:
+                case '0':
+                    print(f'Time: [{self.env.now}] --> state[0] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    yield self.e1
+                    self.state = '1'
 
-            elif self.state == '1':
-                print(f'Time: [{self.env.now}] --> state[1] [x: {self.x:.2f} z: {self.z:.2f}]')
-                yield self.e2
-                self.state='2'
+                case '1':
+                    print(f'Time: [{self.env.now}] --> state[1] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    yield self.e2
+                    self.state = '2'
 
-            elif self.state == '2':
-                print(f'Time: [{self.env.now}] --> state[2] [x: {self.x:.2f} z: {self.z:.2f}]')
-                yield self.e3
-                self.state='3'
+                case '2':
+                    print(f'Time: [{self.env.now}] --> state[2] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    yield self.e3
+                    self.state = '3'
 
+                case '3':
+                    print(f'Time: [{self.env.now}] --> state[3] [x: {self.x:.2f} z: {self.z:.2f}]')
+                    yield self.env.timeout(1)
             
-            elif self.state == '3':
-                print(f'Time: [{self.env.now}] --> state[3] [x: {self.x:.2f} z: {self.z:.2f}]')
-                yield self.env.timeout(0.1)
-
-
+            
 
 def main():
 
@@ -99,7 +98,7 @@ def main():
     env.process(machine.automate())
     env.process(machine.dynamique_continu())
 
-    env.run(until=500)  
+    env.run(until=100)  
 
     time_points = [point[0] for point in machine.x_data]
     x_values = [point[1] for point in machine.x_data]
@@ -108,8 +107,6 @@ def main():
 
        
     fig = plt.figure(figsize=(15, 10))
-
-
     plt.subplot(1, 1, 1)
     plt.plot(x_values, z_values,marker='o')
     plt.xlabel('x values')
